56ec9e5ff9880f3c45fe26004c20ce67c32bb497
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-11-29 13:15:57 -0600

    initial css styles fot post

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,10 +1,10 @@
 <main class="container">
     <section class="section">
         <article class="post">
-            <?php
-                the_title('<h1 class="main-title">', '</h1>');
-                the_content();
-            ?>
+            <?php the_title('<h1 class="main-title">', '</h1>'); ?>
+            <div class="content">
+                <?php the_content(); ?>
+            </div>
         </article>
     </section>
 </main>
