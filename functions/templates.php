2f8aec6a6ed8c6a91a06cdc8d8b72f882d9fdcae
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-11-29 18:25:02 -0600

    change name for css files

--- functions/templates.php
+++ functions/templates.php
@@ -3,7 +3,7 @@
 // Estilos para todos los artículos y páginas
 function single_styles() {
     if ( is_single() or is_page() && ! is_front_page() ) {
-        wp_enqueue_style( 'single-styles', get_template_directory_uri() . '/assets/css/post.css' );
+        wp_enqueue_style( 'single-styles', get_template_directory_uri() . '/assets/css/page.css' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'single_styles' );
