ff73732a82d6d087878504852bb63b64256b7605
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-12-04 12:19:00 -0600

    custom out widget for pages list - delete the woocommerce pages

--- functions/custom-widgets.php
+++ functions/custom-widgets.php
@@ -8,17 +8,18 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
 
     // Obtener la URL de la página actual
     $current_page_url = get_permalink(get_the_ID());
+    $shop_page_url = wc_get_page_permalink('shop');
 
     // Obtener las URLs de las páginas de WooCommerce
-    $wc_pages = [];
-    if (class_exists('WooCommerce')) {
-        $wc_pages = [
-            wc_get_page_permalink('shop'),
-            wc_get_page_permalink('cart'),
-            wc_get_page_permalink('checkout'),
-            wc_get_page_permalink('myaccount'),
-        ];
-    }
+    // $wc_pages = [];
+    // if (class_exists('WooCommerce')) {
+    //     $wc_pages = [
+    //         wc_get_page_permalink('shop'),
+    //         wc_get_page_permalink('cart'),
+    //         wc_get_page_permalink('checkout'),
+    //         wc_get_page_permalink('myaccount'),
+    //     ];
+    // }
 
     $dom = new DOMDocument('1.0', 'UTF-8');
     libxml_use_internal_errors(true);
@@ -33,7 +34,7 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
         }
 
         // Eliminar si el enlace corresponde a una página de WooCommerce
-        elseif ($a && in_array($a->getAttribute('href'), $wc_pages)) {
+        elseif ($a && $a->getAttribute('href') === $shop_page_url) {
             $li->parentNode->removeChild($li);
         }
     }
