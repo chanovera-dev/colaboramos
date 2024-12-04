f5c21e2c5cba749b37248fff23c6066dd638ea77
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-12-04 12:51:08 -0600

    custom out widget for pages list - delete the woocommerce pages

--- functions/custom-widgets.php
+++ functions/custom-widgets.php
@@ -9,9 +9,9 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
     // Obtener la URL de la página actual
     $current_page_url = get_permalink(get_the_ID());
     $shop_page_url = wc_get_page_permalink('shop');
-    $cart_page_url = wc_get_page_permalink('cart');
-    $checkout_page_url = wc_get_page_permalink('checkout');
-    $account_page_url = wc_get_page_permalink('myaccount');
+    // $cart_page_url = wc_get_page_permalink('cart');
+    // $checkout_page_url = wc_get_page_permalink('checkout');
+    // $account_page_url = wc_get_page_permalink('myaccount');
 
     $dom = new DOMDocument('1.0', 'UTF-8');
     libxml_use_internal_errors(true);
@@ -19,7 +19,6 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
 
     foreach ($dom->getElementsByTagName('li') as $li) {
         $a = $li->getElementsByTagName('a')->item(0);
-        $b = $li->getElementsByTagName('a')->item(1);
 
         // Eliminar si el enlace corresponde a la página actual
         if ($a && $a->getAttribute('href') === $current_page_url) {
@@ -27,7 +26,7 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
         }
 
         // Eliminar si el enlace corresponde a una página de WooCommerce
-        elseif ($b && $b->getAttribute('href') === $shop_page_url) {
+        elseif ($a && $a->getAttribute('href') === $shop_page_url) {
             $li->parentNode->removeChild($li);
         }
 
