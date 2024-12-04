fe35eaa404e7088038d8337d33ef1c8a3695b42d
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-12-04 12:54:48 -0600

    custom out widget for pages list - delete the woocommerce pages

--- functions/custom-widgets.php
+++ functions/custom-widgets.php
@@ -8,7 +8,7 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
 
     // Obtener la URL de la página actual
     $current_page_url = get_permalink(get_the_ID());
-    $shop_page_url = wc_get_page_permalink('shop');
+    // $shop_page_url = wc_get_page_permalink('shop');
     // $cart_page_url = wc_get_page_permalink('cart');
     // $checkout_page_url = wc_get_page_permalink('checkout');
     // $account_page_url = wc_get_page_permalink('myaccount');
@@ -26,9 +26,9 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
         }
 
         // Eliminar si el enlace corresponde a una página de WooCommerce
-        elseif ($a && $a->getAttribute('href') === $shop_page_url) {
-            $li->parentNode->removeChild($li);
-        }
+        // elseif ($a && $a->getAttribute('href') === $shop_page_url) {
+        //     $li->parentNode->removeChild($li);
+        // }
 
         // elseif ($a && $a->getAttribute('href') === $cart_page_url) {
         //     $li->parentNode->removeChild($li);
