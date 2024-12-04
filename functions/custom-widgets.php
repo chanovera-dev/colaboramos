6f426b1f1f1cd9d41fde3240442471989fe26025
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-12-04 11:45:24 -0600

    custom out widget for pages list - exclude the woocommerce pages

--- functions/custom-widgets.php
+++ functions/custom-widgets.php
@@ -6,21 +6,7 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
         return $block_content;
     }
 
-    // Obtener la URL de la página actual
     $current_page_url = get_permalink(get_the_ID());
-
-    // Agregar URLs de las páginas de WooCommerce a la lista
-    if (class_exists('WooCommerce')) {
-        $wc_pages = [
-            wc_get_page_permalink('shop'),
-            wc_get_page_permalink('cart'),
-            wc_get_page_permalink('checkout'),
-            wc_get_page_permalink('myaccount'),
-        ];
-        $exclude_urls = array_filter(array_merge([$current_page_url], $wc_pages));
-    } else {
-        $exclude_urls = [$current_page_url];
-    }
 
     $dom = new DOMDocument('1.0', 'UTF-8');
     libxml_use_internal_errors(true);
@@ -28,7 +14,7 @@ function exclude_current_page_and_wc_from_page_list($block_content, $block) {
 
     foreach ($dom->getElementsByTagName('li') as $li) {
         $a = $li->getElementsByTagName('a')->item(0);
-        if ($a && in_array($a->getAttribute('href'), $exclude_urls)) {
+        if ($a && $a->getAttribute('href') === $current_page_url) {
             $li->parentNode->removeChild($li);
         }
     }
