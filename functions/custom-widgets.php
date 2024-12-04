8540a8642d1e2a982431cd50e22d15ea22f3dd81
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-12-04 09:28:51 -0600

    custom widgets for WordPress

--- functions/custom-widgets.php
+++ functions/custom-widgets.php
@@ -1,3 +1,17 @@
 <?php
 
+function exclude_current_page_from_widget($args) {
+    // Obtiene el ID de la página actual.
+    if (is_page()) {
+        $current_page_id = get_queried_object_id();
 
+        // Agrega el ID de la página actual al argumento 'exclude'.
+        if (isset($args['exclude'])) {
+            $args['exclude'] .= ',' . $current_page_id;
+        } else {
+            $args['exclude'] = $current_page_id;
+        }
+    }
+    return $args;
+}
+add_filter('widget_pages_args', 'exclude_current_page_from_widget');
