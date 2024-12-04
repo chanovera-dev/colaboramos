415a59bc2b7280fed9f7bbcff377567b7b93a380
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2024-12-04 13:15:58 -0600

    custom out widget for pages list - exclude the woocommerce pages

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -12,10 +12,18 @@ add_theme_support( 'wc-product-gallery-slider' );
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
 // excluir las páginas de WooCoomerce de las listas de WordPress
-function exclude_woocommerce_pages($query) {
-    if (is_woocommerce()) {
-        return $query;
-    }
-    return $query;
+function exclude_woocommerce_pages_from_page_list_block($query_args) {
+    // Obtiene los IDs de las páginas de WooCommerce
+    $woocommerce_pages = array(
+        get_option('woocommerce_shop_page_id'),
+        get_option('woocommerce_cart_page_id'),
+        get_option('woocommerce_checkout_page_id'),
+        get_option('woocommerce_myaccount_page_id')
+    );
+
+    // Excluye las páginas de WooCommerce
+    $query_args['post__not_in'] = $woocommerce_pages;
+
+    return $query_args;
 }
-add_filter('block_core/page-list', 'exclude_woocommerce_pages');
+add_filter('block_core_page_list_query', 'exclude_woocommerce_pages_from_page_list_block');
