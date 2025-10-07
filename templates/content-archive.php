e0503256211e86e7917aefb8a1acce4f538e2560
Chano Vera <github@chanovera.com>
2025-10-07 11:34:16 -0600

    new start

--- templates/content-archive.php
+++
@@ -1,8 +0,0 @@
-<article class="colaborator-card post">
-    <?php
-        echo the_category();
-        echo '<img class="avatar" src="'; the_post_thumbnail_url( 'media' ); echo '" alt="Foto del colaborador" loading="lazy" width="150" height="150">';
-        the_title('<h2 class="post-title">', '</h2>');
-        the_content();
-    ?>
-</article>
