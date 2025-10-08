f21d4077bd025f20a195baa11ecabe56181ca611
Chano Vera <github@chanovera.com>
2025-10-08 15:50:05 -0600

    add an temporal animation for the page without Featured Image

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -2,6 +2,12 @@
     <article class="page" id="<?php the_ID(); ?>">
         <header class="block">
             <div class="content">
+                <div class="frame">     
+                    <div class="frame-bg">
+                        <img class="bg-color album-artwork" src="https://relatosycartas.com/wp-content/uploads/2025/04/pequena-dama-min.webp"/>
+                        <img class="bg-black album-artwork" src="https://relatosycartas.com/wp-content/uploads/2025/05/Gr_IUuLWoAAmn8Z.jpeg"/>
+                    </div>	
+                </div>
                 <?php
                 if ( has_post_thumbnail() ) {
                         echo get_the_post_thumbnail( null, 'full', [ 'class' => 'background-hero', 'alt'   => get_the_title(), 'loading' => 'lazy', 'data-speed' => '0.25' ] );
