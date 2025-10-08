f1db1a14d4e9c162c8b8a17bf7dc3088ba32e733
Chano Vera <github@chanovera.com>
2025-10-08 13:45:09 -0600

    basic page template - template part for the inter design

---
+++ page.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * Single Page Template
+ * 
+ * @package Colaboramos
+ * @since 1.0.0
+ */
+get_header();
+
+if ( have_posts() ) {
+
+    while( have_posts() ) {
+
+        the_post();
+        get_template_part( 'template-parts/content', 'page' );
+        
+    }
+
+}
+
+get_footer();
