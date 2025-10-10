c9723b154a21606644d0c0a03c4fe1b801140a24
Chano Vera <github@chanovera.com>
2025-10-10 10:26:52 -0600

    add 404 assets and a conditional for related posts

--- inc/templates.php
+++ inc/templates.php
@@ -32,9 +32,9 @@ function colaboramos_enqueue_script( $handle, $path ) {
 }
 
 /**
- * Enqueues styles specifically for Single Page
+ * Enqueues styles specifically for Single Post/Page
  * 
- * Loads custom CSS file only when viewing single page
+ * Loads custom CSS file only when viewing single post/page
  *
  * @since 1.0.0
  * @return void
@@ -45,6 +45,7 @@ function page_template() {
     if ( is_page() or is_single() ) {
         $page_css = "$assets_path/css/page.css";
         $single_css = "$assets_path/css/single.css";
+        $related_css = "$assets_path/css/posts.css";
         $comments_css = "$assets_path/css/comments.css";
         $page_thumbnail = "$assets_path/css/page-thumbnail.css";
         $parallax_hero = "$assets_path/js/parallax-hero.js";
@@ -61,10 +62,20 @@ function page_template() {
 
         if ( is_single() ) {
             colaboramos_enqueue_style( 'single', $single_css );
+            $related = get_posts( [
+                'post__not_in' => [ $post_id ],
+                'posts_per_page' => 1,
+                'category__in' => wp_get_post_categories( $post_id ),
+                'tag__in' => wp_get_post_tags( $post_id, [ 'fields' => 'ids' ] ),
+            ] );
 
-             if ( comments_open() ) {
-                colaboramos_enqueue_style( 'custom-comments', $comments_css );
-             }
+            if ( ! empty( $related ) ) {
+                colaboramos_enqueue_style( 'related-posts', $related_css );
+            }
+
+            if ( comments_open() ) {
+            colaboramos_enqueue_style( 'custom-comments', $comments_css );
+            }
         }
     }
 }
@@ -87,3 +98,22 @@ function posts_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'posts_styles' );
+
+/**
+ * Enqueues styles specifically for 404 error page
+ * 
+ * Loads custom CSS file only when viewing 404 page
+ * to optimize performance and reduce unnecessary loading
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function page404_styles() {
+    $assets_path = '/assets';
+
+    if ( is_404() ) {
+        $error404_css = "$assets_path/css/error404.css";
+        colaboramos_enqueue_style( 'error404', $error404_css );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'page404_styles' );
